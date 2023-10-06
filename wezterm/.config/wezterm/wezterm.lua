d3979c72997890aca5722dee413f79c1be728cee
Max Smirnov <masynchin@gmail.com>
2023-10-07 00:07:57 +0300

    Add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,49 @@
+local wezterm = require "wezterm"
+local act = wezterm.action
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = "Bamboo"
+config.font = wezterm.font("IosevkaTerm Nerd Font", { weight = "Regular" })
+config.font_size = 13.5
+
+config.keys = {
+  -- Pane rotation
+  {
+    key = "LeftArrow",
+    mods = "SHIFT",
+    action = act.ActivatePaneDirection "Left",
+  },
+  {
+    key = "RightArrow",
+    mods = "SHIFT",
+    action = act.ActivatePaneDirection "Right",
+  },
+  {
+    key = "UpArrow",
+    mods = "SHIFT",
+    action = act.ActivatePaneDirection "Up",
+  },
+  {
+    key = "DownArrow",
+    mods = "SHIFT",
+    action = act.ActivatePaneDirection "Down",
+  },
+  -- Pane splitting
+  {
+    key = "\\",
+    mods = "CMD",
+    action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "/",
+    mods = "CMD",
+    action = act.SplitVertical { domain = "CurrentPaneDomain" }
+  }
+}
+
+return config
